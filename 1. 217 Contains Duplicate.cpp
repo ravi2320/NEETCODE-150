1236e00f152910631cb68a6ccd51a7d9fc1d02da
Ravi2320 <rbkadam2320@gmail.com>
2024-01-01 14:07:06 +0530

    1. 217 Contains Duplicate.cpp

---
+++ 1. 217 Contains Duplicate.cpp
@@ -0,0 +1,62 @@
+/*
+217. Contains Duplicate
+Solved
+Easy
+Topics
+Companies
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+*/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        unordered_set<int> hash;
+        for(int i : nums)
+        {
+            if(hash.count(i) > 0)
+            {
+                return true;
+            }
+            hash.insert(i);
+        }
+        return false;
+    }
+};
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        for(int i=1; i<nums.size(); i++)
+        {
+            if(nums[i-1] == nums[i])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
